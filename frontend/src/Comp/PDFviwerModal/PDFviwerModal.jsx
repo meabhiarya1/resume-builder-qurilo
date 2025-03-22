fceb3002fa21c481f8b366f0e42533f9e88fe6f6
Abhishek kumar <abhi.arya97161@gmail.com>
2025-03-22 14:03:53 +0530

    refactor: enhance PDFViewerModal styling; improve layout and responsiveness; add custom scrollbar to image display

--- frontend/src/Comp/PDFviwerModal/PDFviwerModal.jsx
+++ frontend/src/Comp/PDFviwerModal/PDFviwerModal.jsx
@@ -40,8 +40,8 @@ const PDFViewerModal = ({
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50">
-      <div className="relative bg-white rounded-lg shadow-lg w-[80%] max-w-4xl p-6">
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50 ">
+      <div className="relative bg-white rounded-lg shadow-lg w-[80%] max-w-4xl p-6 ">
         {/* Close Button */}
         <button
           onClick={handleCrossButton}
@@ -51,12 +51,12 @@ const PDFViewerModal = ({
         </button>
 
         {/* PDF Viewer */}
-        <p className="text-gray-700 font-semibold flex justify-center ">
+        <p className="mt-4 flex items-center justify-between w-full flex-wrap gap-2 font-semibold mb-2">
           Page {currentPage + 1} of {images.length}
         </p>
         {images.length > 0 ? (
           <div className="flex flex-col items-center max-h-[80vh]">
-            <div className="overflow-auto max-h-[80vh] w-full border rounded-lg p-2">
+            <div className="overflow-auto max-h-[80vh] w-full border rounded-lg p-2 custom-scrollbar">
               <img
                 src={images[currentPage]}
                 alt={`Page ${currentPage + 1}`}
@@ -65,11 +65,11 @@ const PDFViewerModal = ({
             </div>
 
             {/* Page Navigation */}
-            <div className="mt-4 flex items-center justify-between w-full">
+            <div className="mt-4 flex items-center justify-between w-full  ">
               <button
                 onClick={prevPage}
                 disabled={currentPage === 0}
-                className={`px-4 py-2 text-white rounded-lg transition cursor-pointer ${
+                className={`px-2 py-2 text-white rounded-lg transition cursor-pointer text-sm md:py-2 md:px-4 font-semibold ${
                   currentPage === 0
                     ? "bg-gray-400"
                     : "bg-blue-500 hover:bg-blue-700"
@@ -81,7 +81,7 @@ const PDFViewerModal = ({
               <button
                 onClick={handleSave}
                 disabled={saving}
-                className="px-4 py-2 bg-green-500 hover:bg-green-700 text-white rounded-lg transition cursor-pointer"
+                className="px-2 py-2 bg-green-500 hover:bg-green-700 text-white rounded-lg transition cursor-pointer text-sm md:py-2 md:px-4 font-semibold"
               >
                 {saving ? "Saving..." : "Save PDF"}
               </button>
@@ -91,14 +91,14 @@ const PDFViewerModal = ({
                   setImages([]);
                   setShowModal(false);
                 }}
-                className="cursor-pointer px-4 py-2 bg-gradient-to-r from-green-500 to-green-700 hover:from-green-600 hover:to-green-800 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105"
+                className="cursor-pointer px-2 py-2 bg-gradient-to-r from-green-500 to-green-700 hover:from-green-600 hover:to-green-800 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 text-sm md:py-2 md:px-4 "
               >
                 Upload Again
               </button>
               <button
                 onClick={nextPage}
                 disabled={currentPage === images.length - 1}
-                className={`px-4 py-2 text-white rounded-lg transition cursor-pointer ${
+                className={` text-white rounded-lg transition cursor-pointer px-2 py-2 md:py-2 md:px-4 text-sm font-semibold ${
                   currentPage === images.length - 1
                     ? "bg-gray-400"
                     : "bg-blue-500 hover:bg-blue-700"
